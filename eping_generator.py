e32fe7c02e24ceab9f32eb28cff66bf517865ccd
Paulo <ph.lima.oliveira@ufj.edu.br>
2022-05-04 11:54:49 -0300

    Implementado Processamento de Nomes Compostos utilizando NLTK (ATENCAO: aumentar a base de dados de treinamento (arquivo names.json)

--- eping_generator.py
+++ eping_generator.py
@@ -1,3 +1,55 @@
+import random, json
+from flask import jsonify
+
+import torch
+
+from machine_learning.model import NeuralNet
+from machine_learning.nltk_utils import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('machine_learning/names.json', 'r', encoding="utf-8") as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
 class EpingGenerator:
     def generateLogin(nome_completo):
         return str('login.unico')
+
+    def gender_features(word):
+        return {'last_letter': word[-1]}
+
+    def isCompositeName(name):
+        sentence = tokenize(name)
+        if len(sentence) > 1:
+            X = bag_of_words(sentence, all_words)
+            X = X.reshape(1, X.shape[0])
+            X = torch.from_numpy(X).to(device)
+
+            output = model(X)
+            _, predicted = torch.max(output, dim=1)
+
+            tag = tags[predicted.item()]
+
+            probs = torch.softmax(output, dim=1)
+            prob = probs[0][predicted.item()]
+            if prob.item() > 0.99:
+                for intent in intents['intents']:
+                    if tag == intent["tag"]:
+                        return jsonify(f"{random.choice(intent['responses'])}")
+            return jsonify(f"NÃO É UM NOME COMPOSTO")
+        else:
+            return jsonify(f"NÃO É UM NOME COMPOSTO")
--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
+
+import random
+import json
+
+import torch
+
+from machine_learning.model import NeuralNet
+from machine_learning.nltk_utils import bag_of_words, tokenize
+
 from eping_generator import EpingGenerator
 
 app = Flask(__name__)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('machine_learning/names.json', 'r', encoding="utf-8") as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
 
 @app.route("/eping/generate/<nome_completo>", methods=["POST"])
 def generateLogin(nome_completo):
     login_unico = EpingGenerator.generateLogin(nome_completo)
 
     return jsonify({"login": login_unico})
+
+@app.route("/eping/is_composite_name/<nome>", methods=["GET"])
+def isCompositeName(nome):
+    return EpingGenerator.isCompositeName(nome)
 
 if __name__ == '__main__':
     app.run(debug=True)
